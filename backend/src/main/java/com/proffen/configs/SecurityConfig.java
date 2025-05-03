c694539fac3445f17f66949b756abad32f537caf
Roman <roma564888@yandex.ru>
2025-05-03 18:57:58 +0300

    fix: cors disabled

--- backend/src/main/java/com/proffen/configs/SecurityConfig.java
+++ backend/src/main/java/com/proffen/configs/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(exception -> exception
